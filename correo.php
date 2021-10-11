754e3002abf5d78029edc1fae42ee9799708e1f4
nacho diaz <ignaciosergiodiaz@gmail.com>
2021-10-11 18:55:11 -0300

    update correo

--- correo.php
+++ correo.php
@@ -1,12 +1,11 @@
  <?php 
 
     if(isset($_POST['enviar'])){
-        if(!empty($_POST['name']) && !empty($_POST['asunto']) && !empty($_POST['email']) && !empty($_POST['msg'])) {
-            $name = $_POST['name'];
+        if(!empty($_POST['name']) && !empty($_POST['asunto'])  && !empty($_POST['msg']) && !empty($_POST['email'])) {
+            $name = $_POST['name']; 
             $asunto = $_POST['asunto'];
             $msg = $_POST['msg'];
             $email = $_POST['email'];
-
             $header = "From: noreply@example.com" . "\r\n";
             $header .= "Reply-To: noreply@example.com " . "\r\n";
             $header .= "X-Mailer: PHP/" . phpversion(); 
