4297a4bb9ed5f4bcde9ebb335f10353c34487f8d
nacho diaz <ignaciosergiodiaz@gmail.com>
2021-10-11 21:51:49 -0300

    changes in estilos and add new file

--- correo.php
+++ correo.php
@@ -1,19 +1,13 @@
  <?php 
 
-    if(isset($_POST['enviar'])){
-        if(!empty($_POST['name']) && !empty($_POST['asunto'])  && !empty($_POST['msg']) && !empty($_POST['email'])) {
-            $name = $_POST['name']; 
-            $asunto = $_POST['asunto'];
-            $msg = $_POST['msg'];
-            $email = $_POST['email'];
-            $header = "From: noreply@example.com" . "\r\n";
-            $header .= "Reply-To: ignaciosergiodiaz@gmail.com " . "\r\n";
-            $header .= "X-Mailer: PHP/" . phpversion(); 
-            $mail =  mail($name, $email,$asunto,$msg,$header);
-            if($mail){
-                echo "<h2>¡Mail enviado de forma exitosa!<h2>";
-            }
-        }
-    }
+    $destino = "ignaciosergiodiaz@gmail.com";
+    $nombre = $_POST['name'];
+    $asunto = $_POST['asunto'];
+    $msg = $_POST['msg'];
+
+    $contenido = "Nombre:" . $nombre . "\nCorreo: " . $correo . "\nAsunto: " . $asunto . "\nMensaje" . $msg ;
+    mail($destino, "Contacto ", $contenido);
+
+    header("Location:gracias.html");
 
  ?>
